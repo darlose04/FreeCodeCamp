7f9729dee982fee5f575df553b0d2afa889ea0ab
Zach Smith <zsmith126028@gmail.com>
2019-01-26 00:00:17 -0600

    add two new tests

--- Algorithms/IntermediateAlgorithms/test.js
+++ Algorithms/IntermediateAlgorithms/test.js
@@ -7,7 +7,15 @@ it('correctly calculates the sum of the numbers in the range 1 to 4', () => {
     assert.equal(sumNums.sumAll([1, 4]), 10);
 });
 
+it('correctly calculates the sum of the numbers in the range 1 to 4', () => {
+    assert.equal(sumNums.sumAll([5, 10]), 45);
+});
+
 // testing with larger number first
 it('correctly calculates the sum of the numbers in the range 1 to 4', () => {
     assert.equal(sumNums.sumAll([4, 1]), 10);
+});
+
+it('correctly calculates the sum of the numbers in the range 1 to 4', () => {
+    assert.equal(sumNums.sumAll([10, 5]), 45);
 });
