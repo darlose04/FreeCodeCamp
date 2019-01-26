5e63259c287255dc08a3d07fabce74932559f6d6
Zach Smith <zsmith126028@gmail.com>
2019-01-25 23:48:43 -0600

    add tests to check sum of num range in array

--- Algorithms/IntermediateAlgorithms/test.js
+++ Algorithms/IntermediateAlgorithms/test.js
@@ -1,1 +1,12 @@
+const sumNums = require('./sumNumInRange.js');
 const assert = require('assert');
+
+// testing with smaller number first
+it('correctly calculates the sum of the numbers in the range 1 to 4', () => {
+    assert.equal(sumNums.sumAll([1, 4]), 10);
+});
+
+// testing with larger number first
+it('correctly calculates the sum of the numbers in the range 1 to 4', () => {
+    assert.equal(sumNums.sumAll([4, 1]), 10);
+});
