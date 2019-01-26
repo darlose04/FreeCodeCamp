3966ae90799d931d8c7bc340d98110842093277c
Zach Smith <zsmith126028@gmail.com>
2019-01-26 14:52:25 -0600

    add new tests (that don't work)

--- Algorithms/IntermediateAlgorithms/test.js
+++ Algorithms/IntermediateAlgorithms/test.js
@@ -1,4 +1,5 @@
 const sumNums = require('./sumNumInRange.js');
+const diffArray = require('./diffTwoArrays.js');
 const assert = require('assert');
 
 // sumNumInRange
@@ -18,4 +19,13 @@ it('correctly calculates the sum of the numbers in the range 1 to 4', () => {
 
 it('correctly calculates the sum of the numbers in the range 1 to 4', () => {
     assert.equal(sumNums.sumAll([10, 5]), 45);
+});
+
+// difference of two arrays testing
+it('correctly returns an array of the symmetric differences between two arrays', () => {
+    assert.equal(diffArray.diffArray([1,2,3,5], [1,2,3,4,5]), [4]);
+});
+
+it('correctly returns an array of the symmetric differences between two arrays', () => {
+    assert.equal(diffArray.diffArray(["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"]), ["pink wool"]);
 });
