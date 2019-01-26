803b9be8ea9b37c78177a82e09eba63772a65030
Zach Smith <zsmith126028@gmail.com>
2019-01-26 14:52:00 -0600

    add new algo problem to repository

---
+++ Algorithms/IntermediateAlgorithms/diffTwoArrays.js
@@ -0,0 +1,9 @@
+// compare two arrays and return a new array with any items only found in one of the given arrays, but not both. Return the symmetric difference. 
+
+function diffArray(arr1, arr2) {
+    
+}
+
+module.exports = {
+    diffArray
+}
