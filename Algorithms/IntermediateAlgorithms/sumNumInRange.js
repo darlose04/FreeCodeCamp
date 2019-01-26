450f4cb0f7d21ba6fe8abc6a6561a46b490cd382
Zach Smith <zsmith126028@gmail.com>
2019-01-26 00:00:38 -0600

    add code that passes first test

--- Algorithms/IntermediateAlgorithms/sumNumInRange.js
+++ Algorithms/IntermediateAlgorithms/sumNumInRange.js
@@ -2,7 +2,16 @@
 // The lowest number will not always come first.
 
 function sumAll(arr) {
-    
+    let x = arr[0];
+    let y = arr[1];
+    let sum = 0;
+
+    while (x <= y) {
+        sum += x;
+        x++;
+    }
+
+    return sum;
 }
 
 module.exports = {
